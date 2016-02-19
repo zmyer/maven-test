ac0f5b9d0a3a8a946700784490dcb78673240d41
zmyer <zhouwei198732@126.com>
2016-02-19 17:38:55 +0800

    second commit

--- src/main/java/com/zmyer/maven_test/App.java
+++ src/main/java/com/zmyer/maven_test/App.java
@@ -6,22 +6,30 @@ package com.zmyer.maven_test;
  */
 public class App 
 {
-	private final int count = 1000;
-	private int index = 0;
-	public class RunOne implements Runnable
+	public static class RunOne implements Runnable
 	{
 
+		private final int count = 1000;
 		public void run() {
 			// TODO Auto-generated method stub
-			for(int i = 0; i< App.this.count; i++)
+			for(int i = 0; i< count; i++)
 			{
 				System.out.println("asdaf");
 			}
 		}
 		
 	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        App app = new App();
+        RunOne one = new RunOne();
+        Thread thread = new Thread(new RunOne());
+        thread.setDaemon(false);
+        
+        thread.start();
+        
     }
 }
