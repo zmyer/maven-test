c99d472769d2da33533c8e0b22e48fd3beee6817
zmyer <zhouwei198732@126.com>
2016-02-19 17:12:16 +0800

    second commit

--- src/main/java/com/zmyer/maven_test/App.java
+++ src/main/java/com/zmyer/maven_test/App.java
@@ -6,6 +6,20 @@ package com.zmyer.maven_test;
  */
 public class App 
 {
+	private final int count = 1000;
+	private int index = 0;
+	public class RunOne implements Runnable
+	{
+
+		public void run() {
+			// TODO Auto-generated method stub
+			for(int i = 0; i< App.this.count; i++)
+			{
+				
+			}
+		}
+		
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
